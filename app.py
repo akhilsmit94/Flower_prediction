7816b3dab228518484e28f717e2f0896ebdda557
akhilsmit94 <64728625+akhilsmit94@users.noreply.github.com>
2022-02-09 19:08:11 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import tensorflow as tf
+import streamlit as st
+
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+  model=tf.keras.models.load_model('my_model_new.hdf5')
+  return model
+with st.spinner('Model is being loaded..'):
+  model=load_model()
+
+st.write("""
+         # Flower Classification
+         """
+         )
+
+file = st.file_uploader("Please upload flower", type=["jpg", "png"])
+import cv2
+from PIL import Image, ImageOps
+import numpy as np
+st.set_option('deprecation.showfileUploaderEncoding', False)
+def import_and_predict(image_data, model):
+    
+        size = (224,224)    
+        image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+        image = np.asarray(image)
+        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        #img_resize = (cv2.resize(img, dsize=(75, 75),    interpolation=cv2.INTER_CUBIC))/255.
+        
+        img_reshape = img[np.newaxis,...]
+    
+        prediction = model.predict(img_reshape)
+        
+        return prediction
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    predictions = import_and_predict(image, model)
+    class_names=['bougainvillea','daisies','garden_roses','gardenias','hibiscus','hydrangeas','lilies','orchids']
+    string="This image most likely is  "+class_names[np.argmax(predictions)]
+    st.success(string)
+
